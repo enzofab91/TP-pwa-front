42e44e0181aa9393921502ea55da5af821643c7f
fedeziraldo <federrico04@gmail.com>
2019-11-30 17:56:02 -0300

    destacados en home

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,34 @@ import React from 'react';
 class Home extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            destacados: []
+        }
     };
 
+    componentDidMount() {
+        fetch('http://localhost:3000/productos/destacados', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem("token")
+            }
+        }).then(res => res.json())
+        .then(result => { 
+            console.log(result);
+            this.setState({
+                destacados: result.data.docs
+            })
+        }, error => { console.log(error)})
+    }
+
     render() {
+        const destacados = this.state.destacados.map(d => <li>{d.denominacion}</li>)
       return (
         <div className="App">
+            <h2>Productos destacados</h2>
+            <ul>{destacados}</ul>
         </div>
     )};
 }
