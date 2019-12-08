d338a15e753ba18b54941860787e20825f707723
fedeziraldo <federrico04@gmail.com>
2019-12-08 19:40:02 -0300

    detalle producto

--- src/CatalogoProductos.js
+++ src/CatalogoProductos.js
@@ -1,16 +1,76 @@
 import React from 'react';
+import DetalleProducto from './DetalleProducto';
 
 class CatalogoProductos extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            productos: [],
+            page: 1,
+            sort: "",
+            selected : null
+        }
     };
 
+    componentDidMount() {
+        fetch(`http://localhost:3000/productos?sort=${this.state.sort}&page=${this.state.page}`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem("token")
+            }
+        }).then(res => res.json())
+        .then(result => { 
+            console.log(result);
+            this.setState({
+                productos: result.data.docs
+            })
+        }, error => { console.log(error)})
+    }
+
     render() {
+        const productos = this.state.productos.map((p, i) => <tr>
+            <td onClick={(e) => this.verDetalle(i)}>{p.sku}</td>
+            <td>{p.denominacion}</td>
+            <td>{p.descripcion}</td>
+            <td>{p.precio}</td>
+            <td>{p.precioOferta}</td>
+            <td>{p.stock}</td>
+            <td>{p.categoria != null ? p.categoria.nombre : ""}</td>
+            <td><img src="p.imagenes[0]"/></td>
+            <td>{p.denominacion}</td>
+
+        </tr>)
       return (
         <div className="App">
-            <p>PRODUCTOS!!!</p>
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col" >SKU</th>
+                    <th scope="col" >Denominaci&oacute;n</th>
+                    <th scope="col">Descripci&oacute;n</th>
+                    <th scope="col" >Precio</th>
+                    <th scope="col">Oferta</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Im&aacute;genes</th>
+                    <th scope="col">Destacado</th>
+                </tr>
+            </thead>
+            <tbody>
+                {productos}
+            </tbody>
+        </table>
+        <DetalleProducto prod={this.state.selected}></DetalleProducto>
         </div>
     )};
+
+    verDetalle(i) {
+        this.setState({
+            selected : this.state.productos[i]
+        })
+    }
 }
 
 export default CatalogoProductos;
--- src/DetalleProducto.js
+++ src/DetalleProducto.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
-class DetalleProducto extends React.Component {
-    constructor(props){
+class Detalleproducto extends React.Component {
+    constructor(props) {
         super(props)
     };
 
     render() {
-      return (
-        <div className="App">
-        </div>
-    )};
+        let prod = this.props.prod
+        let imagenes
+        let comprar
+        if (prod) {
+            imagenes = prod.imagenes.map(img => <img src={img} />)
+            comprar = <button>Comprar</button>
+        } else prod = {}
+        return (
+            <div className="App" style={{background: "lightblue"}}>
+                    <div>
+                        <p>{prod.sku}</p>
+                        <p>{prod.denominacion}</p>
+                        <p>{prod.descripcion}</p>
+                        <p>{prod.precio}</p>
+                        <p>{prod.precioOferta}</p>
+                        <p>{prod.stock}</p>
+                        <p>{prod.categoria != null ? prod.categoria.nombre : ""}</p>
+                        <p>{imagenes}</p>
+                        <p>{prod.denominacion}</p>
+                    </div>
+                    {comprar}
+            </div>
+        )
+    };
 }
 
-export default DetalleProducto;
+export default Detalleproducto;
